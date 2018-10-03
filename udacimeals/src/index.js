83be62eb5dc01f72b3323ed6608f9a828bf622b1
vinicius p vieira <vinipachecov@gmail.com>
2018-10-02 22:01:36 -0300

    added the react-native app from the beggining of the mobile development part of the course

--- udacimeals/src/index.js
+++ udacimeals/src/index.js
@@ -4,12 +4,28 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import ReduxThunk from 'redux-thunk'
 import reducer from './reducers'
 
+const logger = store => next => action => {
+  console.group(action.type)
+  console.info('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  console.groupEnd(action.type)
+  return result
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ || compose
+
 const store = createStore(reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(...[ReduxThunk,logger])
+  )
 );
+
 
 
 ReactDOM.render(
